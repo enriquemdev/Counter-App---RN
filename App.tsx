d6d23ea44fc51b49f581756547abebc004a8ba45
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-06-08 10:28:42 -0600

    Added shadow styles to counter  button to both platforms

--- App.tsx
+++ App.tsx
@@ -38,7 +38,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#65558F",
     padding: 20,
     position: "absolute",
-    bottom: 20,
-    right: 20,
+    bottom: 30,
+    right: 30,
+
+    /// Shadow styles
+    shadowColor: "#000", // Only this shadow prop works on both platforms
+
+    // ios only
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+
+    // Android equivalent
+    elevation: 3,
   },
 });
