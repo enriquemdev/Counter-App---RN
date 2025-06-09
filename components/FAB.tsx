3767daa4492aed57964e6c03c9ad10635adce8a1
Enrique Muñoz <enriquemunozdev@gmail.com>
2025-06-08 19:48:54 -0600

    detect pressed state on pressale component inside the style attr

--- components/FAB.tsx
+++ components/FAB.tsx
@@ -18,9 +18,10 @@ export default function FAB({
 }: Props) {
   return (
     <Pressable
-      style={[
+      style={({ pressed }) => [
         styles.floatingButton,
         position === "left" ? styles.positionLeft : styles.positionRight,
+        pressed ? { opacity: 0.7 } : { opacity: 1 },
       ]}
       onPress={onPress}
       onLongPress={onLongPress}
